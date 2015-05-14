eb04ace830987ced8401ff725ba9e53c1ef097c1
Edward I Lin <eilin@pc39.cs.ucdavis.edu>
2015-05-13 20:41:09 -0700

    tests all paths. except ownership (no root priv)

--- runtest.py
+++ runtest.py
@@ -1,5 +1,7 @@
 #Author: Edward Lin
 # python script to automate testing student submissions of a program that changes a files privileges.
+# if a program doesn't fail when it should have during a test, it will fail later on and I can see that the error message doesn't match the test.
+# if the program succeeds completely, something has gone terribly wrong.
 
 # Ideal workflow:
 # grader copies student's makefile and runpriv.c to /home/eilin/ecs153_grading/p3_test_dir/
@@ -60,11 +62,11 @@
 #  (5a)program will fail (no group 95 on testing system)
 
 from subprocess import call 
-import os.path #to check for file
+import os #to check for file and change dir
 import sys #for exit call
 
 def sh( command, pw=False):
-	if pw = True:
+	if pw == True:
 		call( command + "< pw", shell=True )
 	else:
 		call( command, shell=True )
@@ -76,7 +78,7 @@ if not os.path.isfile("runpriv"):
 call("mv ./runpriv ./badID/", shell=True)
 
 # script waits for grader to visually inspect c source and change defined ID
-print "inspect c source and change ID"
+print "\n***Inspect c source and change ID***"
 call("gedit ./runpriv.c &", shell=True)
 if raw_input("Continue? (y/n) ") != "y":
 	call("rm /home/eilin/ecs153_grading/p3_test_dir/badID/runpriv", shell=True)
@@ -86,29 +88,55 @@ pw = False
 if raw_input("does it test a password? (y/n) ") == "y":
 	pw = True
 
-# script makes runpriv, copies to /goodID_noSniff/, copies to /goodID_oldSniff/, moves to /goodID_sniff/
+# script makes runpriv, copies to /goodID_noSniff/, copies to /goodID_oldSniff/, move to /goodID_sniff/
 sh("make")
 sh("cp ./runpriv ./goodID_noSniff/")
 sh("cp ./runpriv ./goodID_oldSniff/")
-##call("mv ./runpriv ./goodID_sniff/", shell=True)
+sh("mv ./runpriv ./goodID_sniff/")
 
 # script tests file in /badID/
 #	test, delete runpriv
+print "\nTesting bad ID"
 sh("./badID/runpriv", pw)
 sh("rm ./badID/runpriv")
 
 # script tests file in /goodID_noSniff/
 #	test, delete runpriv
-sh("./goodID_noSniff/runpriv", pw)
+os.chdir("/home/eilin/ecs153_grading/p3_test_dir/goodID_noSniff/")
 if pw:
-	sh("./goodID_noSniff/runpriv < badpw", pw=False) #tests bad password 
-sh("rm ./goodID_noSniff/runpriv")
+	print "\nTesting bad password"
+	sh("./runpriv < badpw", pw=False) #tests bad password
+print "\nTesting no sniff file"
+sh("./runpriv", pw)
+sh("rm ./runpriv")
 
 # script tests file in /goodID_sniff/
-#	create sniff, chmod so user can't execute, test, delete sniff
-#	create sniff, chmod so anyone can read, test, delete sniff
-#	create sniff, chmod so only user can execute, no one else can rwx, test, delete sniff
+#	create sniff, chmod so user can't execute, test
+os.chdir("/home/eilin/ecs153_grading/p3_test_dir/goodID_sniff/")
+sh("touch ./sniff")
+print "\nTesting no user x privilege"
+sh("chmod 400 ./sniff")
+sh("./runpriv", pw)
+#	chmod so anyone can read and write, test
+print "\nTesting others' r privilege"
+sh("chmod 466 ./sniff")
+sh("./runpriv", pw)
+#	chmod so only user can execute, no one else can rwx, test, delete sniff and runpriv
+print "\nTesting all conditions pass (should fail still)"
+sh("chmod 700 ./sniff")
+sh("./runpriv", pw)
+sh("rm -f ./runpriv ./sniff")
+
 # script tests file in /goodID_oldSniff
 #	test, delete runpriv
+print "\nTesting old sniff file"
+os.chdir("/home/eilin/ecs153_grading/p3_test_dir/goodID_oldSniff/")
+sh("./runpriv", pw)
+sh("rm ./runpriv")
+os.chdir("/home/eilin/ecs153_grading/p3_test_dir/")
+
 # script asks to delete makefile and runpriv.c from /home/eilin/ecs153_grading/p3_test_dir/
+if raw_input("\nDelete makefile and runpriv.c? (y/n) ") == "y":
+	sh("rm -v ./*akefile ./runpriv.c")
+
 # script exits
